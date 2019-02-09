4ceae51742a9f101349a531fb6436cce3de261fd
Sean Smith <sean.smith@tenx.tech>
2019-02-10 01:16:25 +0800

    Refactor

--- server.js
+++ server.js
@@ -15,27 +15,35 @@ app.get('/', function(req, res, next){
 // Initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
+// Helper to validate messages
+const messageIsValid = (data) => {
+  return (
+    data.message &&
+    data.message_type &&
+    data.message.id &&
+    data.message.author &&
+    data.message.message &&
+    data.message.uuid
+  );
+};
+
+
 // Run WebSocket connection
 wss.on('connection', (ws) => {
   console.log("Client connected");
-  
-  ws.on('message', function(message) {
+  ws.on('message', (message) => {
     console.log(`Received message => ${message}`);
-
     try {
       const data = JSON.parse(message);
-      if (data.message_type && data.message) {
-        const messageData = data.message;
-        if (messageData.id && messageData.author && messageData.message && messageData.uuid) {
-          /**
-           * Broadcast to all connected clients
-           */
-          wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-              client.send(message);
-            }
-          });
-        }
+      if (messageIsValid(data)) {
+        /**
+         * Broadcast to all connected clients
+         */
+        wss.clients.forEach((client) => {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(message);
+          }
+        });
       }
     } catch (err) {
       console.log("Could not parse message", err);
