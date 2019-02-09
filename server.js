2c548be37987d2b9fba70328b4a59d3aa1e9a33a
Sean Smith <sean.smith@tenx.tech>
2019-02-10 00:13:54 +0800

    Refactor server to broadcast messages

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
 const app = express();
-const expressWs = require('express-ws')(app);
- 
+
+// Initialize a simple http server
+const server = http.createServer(app);
+
+// Create a GET / route
 app.get('/', function(req, res, next){
   res.send("Socket server is running...");
 });
- 
-app.ws('/', function(ws, req) {
+
+// Initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+// Run WebSocket connection
+wss.on('connection', (ws) => {
   console.log("Client connected");
   
   ws.on('message', function(message) {
@@ -17,15 +27,23 @@ app.ws('/', function(ws, req) {
       if (data.message_type && data.message) {
         const messageData = data.message;
         if (messageData.id && messageData.author && messageData.message && messageData.uuid) {
-          ws.send(message);
+          /**
+           * Broadcast to all connected clients
+           */
+          wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+              client.send(message);
+            }
+          });
         }
       }
     } catch (err) {
       console.log("Could not parse message", err);
     }
-  });
+  });  
 });
- 
+
 const PORT = process.env.PORT || 9001;
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+// Start the server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
